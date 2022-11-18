4a7e90a223ebfdb7b6c2329fdf45920be7c4910a
root <cristiano.rosetti@gmail.com>
2022-11-18 04:35:44 +0000

    onboard eks pipeline

--- harness-provision/main.tf
+++ harness-provision/main.tf
@@ -17,6 +17,17 @@ module "bootstrap_harness_projects" {
   harness_platform_projects = local.projects
 }
 
+# Creates Pipelines
+module "bootstrap_harness_pipelines" {
+  depends_on = [
+    module.bootstrap_harness_projects,
+    module.bootstrap_harness_connectors,
+  ]
+  source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-pipeline?ref=main"
+  suffix                     = random_string.suffix.id
+  harness_platform_pipelines = local.pipelines
+}
+
 # Outputs
 output "projects" {
   value = module.bootstrap_harness_projects.project
--- harness-provision/variables.tf
+++ harness-provision/variables.tf
@@ -45,11 +45,15 @@ variable "remote_state" {
 
 # common vars
 locals {
-  common_tags   = { tags = ["owner: ${var.organization_prefix}"] }
-  common_schema = { org_id = data.terraform_remote_state.harness.outputs.organizations[var.organization_prefix].org_id }
+  common_tags = { tags = ["owner: ${var.organization_prefix}"] }
+  common_schema = {
+    org_id = data.terraform_remote_state.harness.outputs.organizations[var.organization_prefix].org_id
+    suffix = random_string.suffix.id
+  }
+  git_suffix = "_github_connector"
 }
 
-# common vars
+# projects vars
 locals {
   projects = { for key, value in var.harness_platform_projects : key => merge(value, local.common_schema) }
 }
@@ -91,5 +95,30 @@ locals {
     local.common_schema
     )
     if details.enable
+  }
+}
+
+# pipelines
+locals {
+  pipelines = { for pipe, values in var.harness_platform_pipelines : pipe => {
+    pipeline = merge(
+      { for key, value in values : key => value if key != "custom_template" },
+      values.custom_template.pipeline,
+      {
+        vars = merge(
+          values.custom_template.pipeline.vars,
+          local.common_schema,
+          {
+            project_id        = module.bootstrap_harness_projects.project[values.custom_template.pipeline.vars.project].identifier
+            git_connector_ref = module.bootstrap_harness_connectors.connectors.github_connectors["${values.custom_template.pipeline.vars.git_connector}${local.git_suffix}"].identifier
+
+            #service_ref       = module.bootstrap_harness_delegates.delegate_init.service_ref
+            #environment_ref   = module.bootstrap_harness_delegates.delegate_init.environment_ref
+          }
+        )
+      }
+    )
+    inputset = { for input, details in try(values.custom_template.inputset, {}) : input => merge(details) if details.enable }
+    } if pipe != "harness_seed_setup"
   }
 }
