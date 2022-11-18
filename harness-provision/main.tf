b1b48a5edb2953ff786a110a5fae4cd17a03a509
root <cristiano.rosetti@gmail.com>
2022-11-18 03:04:50 +0000

    adding infra connectors

--- harness-provision/main.tf
+++ harness-provision/main.tf
@@ -1,3 +1,15 @@
+# Creates and Setup Harness connectors
+module "bootstrap_harness_connectors" {
+  source                             = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=main"
+  suffix                             = random_string.suffix.id
+  harness_platform_github_connectors = local.github_connectors
+  harness_platform_docker_connectors = local.docker_connectors
+  harness_platform_aws_connectors    = local.aws_connectors
+
+  # requires to provision first the infra
+  # harness_platform_k8s_connectors    = local.k8s_connectors
+}
+
 # Create Projects
 module "bootstrap_harness_projects" {
   source                    = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-project?ref=main"
@@ -5,8 +17,10 @@ module "bootstrap_harness_projects" {
   harness_platform_projects = local.projects
 }
 
-
 # Outputs
 output "projects" {
   value = module.bootstrap_harness_projects.project
+}
+output "connectors" {
+  value = module.bootstrap_harness_connectors.connectors
 }
--- harness-provision/variables.tf
+++ harness-provision/variables.tf
@@ -1,15 +1,95 @@
-variable "harness_platform_account_id" {}
+# harness variables
 variable "harness_platform_api_key" {
+  type      = string
   sensitive = true
 }
-variable "harness_platform_projects" {}
-variable "harness_platform_pipelines" {}
-variable "harness_platform_delegates" {}
-variable "harness_platform_github_connectors" {}
-variable "organization_prefix" {}
-variable "remote_state" {}
+variable "harness_platform_account_id" {
+  type = string
+}
+variable "harness_platform_projects" {
+  type = map(any)
+}
+variable "harness_platform_delegates" {
+  type    = map(any)
+  default = {}
+}
+variable "harness_platform_github_connectors" {
+  type    = map(any)
+  default = {}
+}
+variable "harness_platform_docker_connectors" {
+  type    = map(any)
+  default = {}
+}
+variable "harness_platform_aws_connectors" {
+  type    = map(any)
+  default = {}
+}
+variable "harness_platform_pipelines" {
+  type    = map(any)
+  default = {}
+}
+variable "harness_platform_inputsets" {
+  type    = map(any)
+  default = {}
+}
+# other variables
+variable "organization_prefix" {
+  type    = string
+  default = ""
+}
+variable "remote_state" {
+  # type    = map(any)
+  default = {}
+}
 
+# common vars
 locals {
-  org_id   = data.terraform_remote_state.harness.outputs.organizations[var.organization_prefix].org_id
-  projects = { for key, value in var.harness_platform_projects : key => merge(value, { org_id = local.org_id }) }
+  common_tags   = { tags = ["owner: ${var.organization_prefix}"] }
+  common_schema = { org_id = data.terraform_remote_state.harness.outputs.organizations[var.organization_prefix].org_id }
+}
+
+# common vars
+locals {
+  projects = { for key, value in var.harness_platform_projects : key => merge(value, local.common_schema) }
+}
+
+# github connectors
+locals {
+  github_connectors = { for name, details in var.harness_platform_github_connectors : name => merge(
+    details,
+    local.common_tags,
+    local.common_schema,
+    {
+      validation_repo = details.connection_type == "Repo" ? "" : details.validation_repo
+      project_id      = try(details.project_id, "")
+      credentials = {
+        http = {
+          username     = details.credentials.http.username
+          token_ref_id = try(details.credentials.http.token_ref_id, "")
+        }
+      }
+      api_authentication = {
+        token_ref = try(details.credentials.http.token_ref_id, "")
+      }
+  }) }
+}
+
+# connectors
+locals {
+  docker_connectors = { for name, details in var.harness_platform_docker_connectors : name => merge(
+    details,
+    local.common_tags,
+    local.common_schema
+    )
+    if details.enable
+  }
+
+  aws_connectors = { for name, details in var.harness_platform_aws_connectors : name => merge(
+    details,
+    local.common_tags,
+    local.common_schema
+    )
+    if details.enable
+  }
 }
